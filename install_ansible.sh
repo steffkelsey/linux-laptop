e15eb577aa121fed158b4c0b60eab2ae56c82840
steffkelsey <thecodeviking@gmail.com>
2019-05-29 17:28:47 -0400

    Updated to work on Debian

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Installs Ansible
-# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-ubuntu
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-debian
 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
